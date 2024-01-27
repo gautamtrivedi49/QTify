36f70a21590bfc1717d974f8f1b194941e4d7b92
Gautam Trivedi <gautamtrivedi49@gmail.com>
2024-01-27 22:06:09 +0530

    Module-1

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,19 +1,11 @@
-import React from "react";
-import logo from "../../assets/logo.svg";
-import styles from './Logo.module.css';
-
 const Logo = () => {
   return (
-    <>
-      <img
-        src={logo}
-        alt="logo"
-        width={67}
-        height={34}
-        className={styles.Logo}
-      />
-    </>
+    <div>
+      <img src="./Logo.png" alt="Logo" />
+    </div>
   );
 };
 
 export default Logo;
+
+
